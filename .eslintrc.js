e0beeffb2c92dde17baf2e72c19543e55d42c178
Hieu Tran <hieu.tran1@ncc.asia>
2023-03-23 02:07:27 +0700

    config eslintc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'standard-with-typescript'
-	],
-	overrides: [
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		project: ['./tsconfig.json']
-	},
-	plugins: [
-		'react'
-	],
-	rules: {
-		indent: 'off',
-		semi: ['error', 'always'],
-		'@typescript-eslint/semi': 'off'
+env: {
+    browser: true,
+    es2021: true,
+    node: true
+},
+extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+],
+overrides: [
+],
+parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+},
+plugins: [
+    'react'
+],
+rules: {
+    indent: 'off',
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': 'off'
 
-	}
+}
 };
